aabede8c782b6433ffef723c319b85afad76c8a9
dimitrich212003 <ytrewq212003@mail.ru>
2023-10-26 08:47:43 +0300

    add summary to Main.java

--- .idea/src/Main.java
+++ .idea/src/Main.java
@@ -1,5 +1,8 @@
 class Main{
     public static void main(String[] args) {
         System.out.print("Это добавлено в ветке main");
+        int a = 1;
+        int b = 2;
+        System.out.print(a + b);
     }
 }
